993b5c6376d5e17c7fb66267d8ab9fb1ef234ff8
Fredi Karu <fredikaru@gmail.com>
2024-05-23 03:01:33 +0300

    Reload page after save

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -6,7 +6,7 @@ const inputClass =
   "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white";
 const labelClass = "block text-sm font-medium text-gray-900 dark:text-white";
 
-export const EditModal = () => {
+export const EditModal = ({ reloadPage }) => {
   const { boardId } = useParams();
   const { taskTitle } = useParams();
   const [oldTask, setOldTask] = useState("");
@@ -28,6 +28,7 @@ export const EditModal = () => {
 
   function handleSave() {
     saveTask(oldTask, formData);
+    reloadPage();
   }
 
   // useEffect(() => {
