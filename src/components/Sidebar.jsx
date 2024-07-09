5677c8c52f0be03b6121cd9861a7e7eb7fe92f98
Fredi Karu <fredikaru@gmail.com>
2024-07-10 01:11:19 +0300

    Revert to jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 
 import boardIcon from "../assets/icon-board.svg";
+import { useTasks } from "../hooks/useTasks";
 
-function BoardListItem({ board, selectedBoard, selectBoard }) {
-  return (
-    <div className="">
-      <div
-        onClick={() => selectBoard(board.name)}
-        className={
-          board.name === selectedBoard.name
-            ? "text-white bg-purple pl-3 py-2 flex rounded-r-full"
-            : "text-gra pl-3 py-2 flex"
-        }
-      >
-        <img src={boardIcon} alt="Board Icon" />
-        <p className="ml-1">{board.name}</p>
-      </div>
-    </div>
-  );
-}
+// function BoardListItem({ board, currentBoard }) {
+//   return (
+//     <div
+//       className=""
+//       className={
+//         board.name === currentBoard
+//           ? "text-white bg-purple pl-3 py-2 flex rounded-r-full"
+//           : "text-gray pl-3 py-2 flex"
+//       }
+//     >
+//       <div onClick={() => console.log("yes")}>
+//         <img src={boardIcon} alt="Board Icon" />
+//         <p className="ml-1">{board.name}</p>
+//       </div>
+//     </div>
+//   );
+// }
 
-export const Sidebar = ({ data, selectBoard, selectedBoard }) => {
+export const Sidebar = ({ boards }) => {
+  // const { boardsData, currentBoard, setCurrentBoard } = useTasks();
+  // console.log("here");
+  // console.log(boardsData);
+
   return (
     <div className=" text-gray">
-      <p>ALL BOARDS ({data.boards.length})</p>
-      {data.boards.map((board) => (
-        <BoardListItem
-          key={board.name}
-          board={board}
-          selectedBoard={selectedBoard}
-          selectBoard={selectBoard}
-        />
+      <p>ALL BOARDS {boards.length}</p>
+      {boards.map((board) => (
+        <h2>hey</h2>
       ))}
       <a href="#" className="text-purple">
         <img src={boardIcon} style={{ fill: "#FFFFFF" }} alt="Board Icon" /> +
